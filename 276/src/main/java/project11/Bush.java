42167f75f1ab63ebb080381cbc121d629a4227b4
eca162 <ekansh_chawla_2@sfu.ca>
2024-10-27 02:27:14 -0700

    added classes for CellArray and GameObject and removed redundant factory methods

--- 276/src/main/java/project11/Bush.java
+++ 276/src/main/java/project11/Bush.java
@@ -3,13 +3,21 @@ package project11;
 public class Bush extends GameObject {
     private boolean occupied;
 
-    public Bush() {
-        this.solid = false;
+    public Bush(int x, int y, boolean occupied) {
+        super(x, y, occupied);
+        this.solid = false; // Bush is not solid by default
+        this.occupied = occupied;
     }
 
     @Override
     public void render() {
+        // Implement rendering logic for Bush
         System.out.println("Rendering Bush at (" + x + ", " + y + ")");
+    }
+
+    @Override
+    public boolean isSolid() {
+        return solid;
     }
 
     public boolean isOccupied() {
--- 276/src/main/java/project11/GameObjectFactory.java
+++ 276/src/main/java/project11/GameObjectFactory.java
@@ -1,5 +1,15 @@
 package project11;
 
-public abstract class GameObjectFactory {
-    public abstract GameObject createGameObject();
+public class GameObjectFactory {
+
+    public GameObject createObject(String type) {
+        switch (type.toLowerCase()) {
+            case "bush":
+                return new Bush(0, 0, false); // Creates a Bush with default position and not occupied
+            case "barrier":
+                return new Barrier(0, 0); // Creates a Barrier with default position
+            default:
+                throw new IllegalArgumentException("Unknown GameObject type: " + type);
+        }
+    }
 }
