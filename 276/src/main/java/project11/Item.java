1955e8382a979da12622a610da507ed0edbe0f39
Felmer Macadangdang <felmermac@gmail.com>
2024-10-26 18:29:32 -0700

    Changed Item.java

--- 276/src/main/java/project11/Item.java
+++ 276/src/main/java/project11/Item.java
@@ -4,14 +4,20 @@ package project11;
  * Item class used to represent the items in the game.
  * Extends GameObject class.
  * 
+ * @author Felmer M
  */
+
 public abstract class Item extends GameObject {
-    public  Item(int x, int y) {
+    /**
+     * Constructor for the Item class.
+     * Solid is set to false.
+     * 
+     * @param x initializes the x position of the item.
+     * @param y initializes the y position of the item.
+     */
+    public Item(int x, int y) {
         this.x = x;
         this.y = y;
-        this.solid = false;
+        this.solid = false; 
     }
-
-    @Override
-    public abstract void render();
 }
