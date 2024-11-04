f02c3afd0aabb91563984782d183027a6b1c7ba0
eca162 <ekansh_chawla_2@sfu.ca>
2024-11-04 15:31:26 -0800

    removed individual rendering of objects

--- 276/src/main/java/project11/Hole.java
+++ 276/src/main/java/project11/Hole.java
@@ -10,32 +10,11 @@ public class Hole extends GameObject {
 
     public Hole(int x, int y, boolean solid, int typeId) {
         super(x, y, solid, typeId);
-        this.solid = false; // Hole is not solid by default
-        loadSprite();
     }
 
-    // Load the hole sprite once
-    private void loadSprite() {
-        if (holeSprite == null) {
-            try {
-                holeSprite = ImageIO.read(getClass().getResource("/project11/sprites/Hole.png"));
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println("Error: Could not load hole sprite.");
-            }
-        }
-    }
+    
 
-    @Override
-    public void render(Graphics g, int tileSize) {
-        if (holeSprite != null) {
-            g.drawImage(holeSprite, x * tileSize, y * tileSize, tileSize, tileSize, null);
-        } else {
-            // Fallback if sprite fails to load
-            g.setColor(java.awt.Color.BLACK);
-            g.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
-        }
-    }
+    
 
     @Override
     public boolean isSolid() {
