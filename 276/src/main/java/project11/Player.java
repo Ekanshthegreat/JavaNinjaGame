0995032dd290f0bf63e31df28fc1301496e7ba2b
eca162 <ekansh_chawla_2@sfu.ca>
2024-11-04 17:28:36 -0800

    fixed obj

--- 276/src/main/java/project11/Player.java
+++ 276/src/main/java/project11/Player.java
@@ -1,25 +1,12 @@
 package project11;
 
-
-public class Player extends GameObject{
-    // Player attributes like position, score, etc.
-    private int score;
-
-    public Player(int x, int y, int typeId) {
-        super(x, y, true, typeId);
-    }
-
-    public void pickupItem(Pickupable item) {
-        item.onPickup(); // Call onPickup to handle the item pickup logic
-        if (item instanceof Item) {
-            score += ((Item) item).getScore(); // Increase score based on the item's score
-
-public class Player implements Movable {
+public class Player extends GameObject implements Movable {
     private int health;
     private Pickupable[] inventory;
     private boolean canVault;
 
-    public Player() {
+    public Player(int x, int y) {
+        super(x, y, false, 1); // Assuming Player is not solid by default, with typeId 1
         this.health = 100; 
         this.inventory = new Pickupable[10]; 
         this.canVault = false;
@@ -41,30 +28,30 @@ public class Player implements Movable {
         // Handle player death logic
     }
 
+    @Override
+    public void move(int deltaX, int deltaY) {
+        // Implement movement logic for the player
+        this.x += deltaX; // Update player's x position
+        this.y += deltaY; // Update player's y position
+        System.out.println("Moving player to (" + this.x + ", " + this.y + ")");
+    }
+
     public void movePlayer(String direction) {
         switch (direction.toLowerCase()) {
             case "up":
-                move(0, -1);
+                move(0, -1); // Move up
                 break;
             case "down":
-                move(0, 1);
+                move(0, 1); // Move down
                 break;
             case "left":
-                move(-1, 0);
+                move(-1, 0); // Move left
                 break;
             case "right":
-                move(1, 0);
+                move(1, 0); // Move right
                 break;
             default:
                 System.out.println("Invalid direction");
-
         }
-    }
-
-    @Override
-    public void move(int x, int y) {
-        // Implement movement logic for the player
-        System.out.println("Moving player to (" + x + ", " + y + ")");
-        // Update player position
     }
 }
