14d95f7275e8c0cb1d54492b5a7a916aa3f93486
Felmer Macadangdang <felmermac@gmail.com>
2024-11-04 20:09:31 -0800

    updated randomizers

--- 276/src/main/java/project11/MazeBuilder.java
+++ 276/src/main/java/project11/MazeBuilder.java
@@ -99,8 +99,8 @@ public class MazeBuilder {
         int x,y;
         // Randomly choose null to be keys
         while (keyCount <= maxKeys) {
-            x = (int)(Math.random() * (cols - 2)) + 1;
-            y= (int)(Math.random() * (rows - 2)) + 1;
+            x = (int)(Math.random() * cols + 1);
+            y= (int)(Math.random() * rows + 1);
             if (maze[y][x] == null) {
                 maze[y][x] = factory.createObject("key", x, y);
                 keyCount++;
@@ -115,9 +115,9 @@ public class MazeBuilder {
 
         while (holeCount < maxHoles) {
             // Choose y avoiding the outer walls
-            int y = (int)(Math.random() * (rows - 2)) + 1;
+            int y = (int)(Math.random() * rows + 1);
             // Choose x avoiding the outer walls
-            int x = (int)(Math.random() * (cols - 2)) + 1;
+            int x = (int)(Math.random() * cols + 1);
 
 
             if (maze[y][x] != null && maze[y][x].typeId == 6) { // Assuming typeId 6 indicates a wall
@@ -133,11 +133,11 @@ public class MazeBuilder {
 
         while (bushCount < maxBushes) {
             // Choose y avoiding the outer walls
-            int y = (int)(Math.random() * (rows - 2)) + 1;
+            int y = (int)(Math.random() * rows) + 1;
             // Choose x avoiding the outer walls
-            int x = (int)(Math.random() * (cols - 2)) + 1;
+            int x = (int)(Math.random() * cols) + 1;
 
-            if (maze[y][x] != null && maze[y][x].typeId == 6) { // Assuming typeId 6 indicates a wall
+            if (maze[y][x] != null && maze[y][x].typeId == 6) { 
                 maze[y][x] = factory.createObject("bush", x, y);
                 bushCount++;
             }
