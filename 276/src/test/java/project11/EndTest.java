a549b8008fd33447b4b16445bac565ef6468496e
felmerm <felmer2010@yahoo.ca>
2024-11-22 21:05:46 -0800

    added one more skeleton test

--- 276/src/test/java/project11/EndTest.java
+++ 276/src/test/java/project11/EndTest.java
@@ -1,17 +1,18 @@
 package project11;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-class WallTest {
+class EndTest {
 
     @Test
-    void testWallProperties() {
-        Wall Wall = new Wall(0,0);
+    void testEndProperties() {
+        End End = new End(0,0);
 
-        assertTrue(Wall.isSolid(), "Wall should be solid");
-        assertEquals(0, Wall.getX(), "X coordinate should be correct");
-        assertEquals(0, Wall.getY(), "Y coordinate should be correct");
-        assertEquals(6, Wall.getTypeId(), "Type ID should be correct");
+        assertTrue(!End.isSolid(), "End should be solid");
+        assertEquals(0, End.getX(), "X coordinate should be correct");
+        assertEquals(0, End.getY(), "Y coordinate should be correct");
+        assertEquals(6, End.getTypeId(), "Type ID should be correct");
     }
 }
